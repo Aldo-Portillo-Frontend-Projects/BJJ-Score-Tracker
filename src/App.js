353054e6f6caa37fef6bb129e4380f2e9c89e0ac
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-24 10:09:41 -0500

    Add countdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Player from './Player';
 
+import Countdown from 'react-countdown';
+
 function App() {
   return (
     <div className="App">
       <h1>BJJ Score Tracker</h1>
+      <Countdown date={Date.now() + 300000} />
+      
       <Player player="Player 1" />
       <Player player="Player 2" />
+
+      
     </div>
   );
 }
