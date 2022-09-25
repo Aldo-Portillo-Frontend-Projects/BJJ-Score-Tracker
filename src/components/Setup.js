a129233e276a679989e1632d25b3682599139bb8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-25 15:39:01 -0500

    Create form

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -5,8 +5,14 @@ function Setup () {
         <div className='setup'>
             <h1 styles={{color: "black"}}>Setup Render</h1>
             <form>
-            <label>Player 1 Name:
+            <label>Player 1:
                 <input type="text" />
+            </label>
+            <label>Player 2:
+                <input type="text" />
+            </label>
+            <label>Time (in minutes):
+                <input type="number" />
             </label>
             </form>
         </div>
